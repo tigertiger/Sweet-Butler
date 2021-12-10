0e1880f3b8e7b5f1a0c0d5e525f4367b40653b65
Melissa <melissa.scmi@gmail.com>
2021-12-10 15:47:08 -0800

    basic add to list with state on new app

--- sweet-butler-app-2/src/App.js
+++ sweet-butler-app-2/src/App.js
@@ -1,15 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState(['😠 Ugh.', '😠 Grr.']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    console.log("😠");
+    setTodos([...todos, input])
+  }
+
   return (
     <div className="App">
       <h1>Ugh. 😠</h1>
-      <input /><br />
-      <button>Add To Do</button>
-
+      <form>
+        {/* Added form to enable Enter as submit. A much easier way than creating the keylogger...buuuuut...not as cool. :D */}
+        <input value={input} onChange={event => setInput(event.target.value)} /><br />
+        <button type="submit" onClick={addTodo}>Add To Do</button>
+      </form>
       <ul>
-        <li>😠 Ugh.</li>
-        <li>😠 Grr.</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
