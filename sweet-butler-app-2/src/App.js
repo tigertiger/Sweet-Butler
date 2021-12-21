7b0bef904a57e29002a0240163c24710c03558e2
Melissa <melissa.scmi@gmail.com>
2021-12-21 11:40:45 -0800

    move primary to do list into ButlerControl

--- sweet-butler-app-2/src/App.js
+++ sweet-butler-app-2/src/App.js
@@ -4,47 +4,13 @@ import {Button, FormControl, InputLabel, Input} from '@material-ui/core';
 import Todo from './components/Todo';
 import db from './firebase';
 import firebase from 'firebase';
+import ButlerControl from './components/ButlerControl';
 
 function App() {
 
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-
-  useEffect(() => {
-    db.collection('todos').orderBy("timestamp", "desc").onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
-    })
-  }, []);
-
-  const addTodo = (event) => {
-    event.preventDefault();
-
-    db.collection('todos').add({
-      todo: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
-
-    setTodos([...todos, input]);
-    setInput('');
-  }
-
   return (
     <div className="App">
-      <h1>Ugh. 😠</h1>
-      <form>
-      <FormControl>
-        {/* Added form to enable Enter as submit. A much easier way than creating the keylogger...buuuuut...not as cool. :D */}
-        <InputLabel>🎩 How Can I Help?</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)} /><br />
-      </FormControl><br />
-        <Button disabled={!input} variant="outlined" color="default" type="submit" onClick={addTodo}>Add To Do</Button>
-      </form>
-
-      <ul>
-        {todos.map(todo => (
-          <Todo todo={todo} />
-        ))}
-      </ul>
+      <ButlerControl />
     </div>
   );
 }
--- sweet-butler-app-2/src/components/ButlerControl.js
+++ sweet-butler-app-2/src/components/ButlerControl.js
@@ -1,11 +1,16 @@
+// import React, { useState, useEffect } from 'react';
+// import './../App.css'
+// import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, Input } from '@material-ui/core';
+// import Todo from './../components/Todo';
+// import db from './../firebase';
+// import firebase from 'firebase/compat/app';
+import Query from './../components/Query';
+// import "firebase/firestore";
 import React, { useState, useEffect } from 'react';
-import './../App.css'
-import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, Input } from '@material-ui/core';
+import {Button, Checkbox, FormControl, FormControlLabel, InputLabel, Input} from '@material-ui/core';
 import Todo from './../components/Todo';
 import db from './../firebase';
-import firebase from 'firebase/compat/app';
-import Query from './../components/Query';
-import "firebase/firestore";
+import firebase from 'firebase';
 
 function ButlerControl() {
 
