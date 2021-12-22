9b231cabf93b22928e11c6fe05a0e9f89dff9f06
Melissa <melissa.scmi@gmail.com>
2021-12-22 11:52:18 -0800

    update splash fade to include question modal button

--- sweet-butler-app-2/src/components/Greeting.js
+++ sweet-butler-app-2/src/components/Greeting.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Questions from './Questions';
 
 const Greeting = ({ text, text2 }) => {
 
@@ -8,6 +9,10 @@ const Greeting = ({ text, text2 }) => {
   });
 
   const [fadeProp2, setFadeProp2] = useState({
+    fade: 'fade-out',
+  });
+
+  const [fadeProp3, setFadeProp3] = useState({
     fade: 'fade-out',
   });
 
@@ -23,15 +28,21 @@ const Greeting = ({ text, text2 }) => {
           fade: 'fade-in'
         })
       }
+      if (fadeProp2.fade === 'fade-in') {
+        setFadeProp3({
+          fade: 'fade-in'
+        })
+      }
     }, 1000)
     return () => clearInterval(timeout)
-  }, [fadeProp])
+  }, [fadeProp, fadeProp2])
 
   return (
     <>
 
         <h1 id="greebing1" className={fadeProp.fade}>{text}</h1>
         <h1 id="greebing2" className={fadeProp2.fade}>{text2}</h1>
+        <h1 className={fadeProp3.fade}><Questions /></h1>
 
     </>
   )
--- sweet-butler-app-2/src/pages/Welcome.js
+++ sweet-butler-app-2/src/pages/Welcome.js
@@ -8,9 +8,6 @@ function Welcome() {
     <div>
       <Greeting text2="How Are You Feeling?" />
     </div>
-    <div>
-      <Questions />
-    </div>
     </>
   )
 }
